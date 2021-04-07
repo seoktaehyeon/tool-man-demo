92d7246d55d75be9f665ef2eff8dfe9938b8b883
will.shi <seoktaehyeon@msn.com>
2021-04-07 11:21:41 +0800

    debug jenkins

--- jira_client.sh
+++ jira_client.sh
@@ -5,7 +5,7 @@ JIRA_URL=${1?}
 JIRA_USER=${2?}
 JIRA_PASS=${3?}
 JIRA_COMMENT=${4?}
-if $(echo ${JIRA_KEY} | awk -F '-' '{print $NF}' | grep -w '^[0-9]*') == 0; then
+if (( $(echo ${JIRA_KEY} | awk -F '-' '{print $NF}' | grep -w '^[0-9]*') == 0 )); then
     ${CURL} -u ${JIRA_USER}:${JIRA_PASS} "${JIRA_URL}/rest/api/2/issue/$JIRA_KEY/comment" \
         -d "{\"body\": \"${JIRA_COMMENT}\"}"
 fi
