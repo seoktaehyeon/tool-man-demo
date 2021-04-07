57ae281f9267c78f1fc567aedc14c9aa70bd88ce
will.shi <seoktaehyeon@msn.com>
2021-04-07 11:54:59 +0800

    DEMODEV-15 #comment 我在 Wed Apr  7 11:54:59     2021 修复了一个问题

--- jira_client.sh
+++ jira_client.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-CURL="curl -v -X POST -H 'Content-Type: application/json' -H 'Accept: application/json'"
+
 JIRA_KEY=$(git log -n 1 | tail -1 | awk '{print $1}')
 JIRA_URL=${1?}
 JIRA_USER=${2?}
 JIRA_PASS=${3?}
 JIRA_COMMENT=${4?}
+
 echo ${JIRA_KEY} | awk -F '-' '{print $NF}' | grep -w '^[0-9]*' || {
     echo "No jira key in ${JIRA_KEY}"
     echo "Skip jira update"
     exit 0
 }
+
 echo "Update jira comment for ${JIRA_KEY}"
-${CURL} -u ${JIRA_USER}:${JIRA_PASS} "${JIRA_URL}/rest/api/2/issue/$JIRA_KEY/comment" -d "{\"body\": \"${JIRA_COMMENT}\"}"
+curl -sv -H "Content-Type: application/json" -H "Accept: application/json" \
+    -u ${JIRA_USER}:${JIRA_PASS} "${JIRA_URL}/rest/api/2/issue/$JIRA_KEY/comment" \
+    -d "{\"body\": \"${JIRA_COMMENT}\"}"
